64927848a08186f6526e3b70f897fbd266e0ba90
Thierry hentic <thierry.hentic@gmail.com>
2026-09-11 07:12:29 +0200

    Initial Commit

---
+++ Planification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planification
+//
+//  Created by thierryH24 on 11/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planification/PlanificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanificationApp.swift
+//  Planification
+//
+//  Created by thierryH24 on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlanificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
